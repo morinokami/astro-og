632798547d1d090ccda276fc7c8c3b7e158f4fcc
Shinya Fujino <shf0811@gmail.com>
2025-05-04 16:08:56 +0900

    oops

--- packages/astro-og/src/components/x/x-preview.tsx
+++ packages/astro-og/src/components/x/x-preview.tsx
@@ -14,6 +14,7 @@ export function XPreview({
 		props["twitter:description"] ??
 		props["og:description"] ??
 		props.description;
+
 	return cardType === undefined ? (
 		<NoPreview />
 	) : cardType === "summary" || image === undefined ? (
